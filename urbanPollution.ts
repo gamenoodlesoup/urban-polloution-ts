84996e8f3fdd457912b00776f2740b02124a5357
JimmyLeung-GNS <jimmy@gamenoodlesoup.com>
2026-01-15 01:32:31 +0800

    Update pxt.json version to 0.0.10 and refactor agent movement logic in urbanPollution.ts

--- urbanPollution.ts
+++ urbanPollution.ts
@@ -1,39 +1,11 @@
-// enums
-enum Direction {
-  //% block="forward"
-  FORWARD,
-  //% block="back"
-  BACK,
-  //% block="left"
-  LEFT,
-  //% block="right"
-  RIGHT,
-  //% block="up"
-  UP,
-  //% block="down"
-  DOWN,
-}
-
-enum TurnDirection {
-  //% block="left"
-  LEFT_TURN,
-  //% block="right"
-  RIGHT_TURN,
-}
-// global variables
-const directions = [FORWARD, BACK, LEFT, RIGHT, UP, DOWN];
-
-const turns = [LEFT_TURN, RIGHT_TURN];
-
 //% block="Urban Pollution" weight=200 color=#FF0000 icon="\uf1b9"
 namespace urbanPollution {
   /**
    * Move Agent n spaces in the d direction
    */
   //% block="Move %d by %n"
-  export function moveAgent(d: Direction, n: number): void {
-    const direction = directions[d];
-    agent.move(direction, n);
+  export function moveAgent(d: SixDirection, n: number): void {
+    agent.move(d, n);
   }
 
   /**
@@ -41,7 +13,6 @@ namespace urbanPollution {
    */
   //% block="Turn %t"
   export function turnAgent(t: TurnDirection): void {
-    const turn = turns[t];
-    agent.turn(turn);
+    agent.turn(t);
   }
 }
