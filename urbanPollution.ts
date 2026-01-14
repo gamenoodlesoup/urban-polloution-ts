a3b088fdb360e186106b821ec642a4f5af65cf65
JimmyLeung-GNS <jimmy@gamenoodlesoup.com>
2026-01-14 20:45:24 +0800

    Refactor urbanPollution.ts for improved formatting and consistency

--- urbanPollution.ts
+++ urbanPollution.ts
@@ -1,53 +1,40 @@
 // enums
 enum Direction {
-    //% block="forward"
-    Forward,
-    //% block="back"
-    Back,
-    //% block="left"
-    Left,
-    //% block="right"
-    Right,
-    //% block="up"
-    Up,
-    //% block="down"
-    Down
+  //% block="forward"
+  Forward,
+  //% block="back"
+  Back,
+  //% block="left"
+  Left,
+  //% block="right"
+  Right,
+  //% block="up"
+  Up,
+  //% block="down"
+  Down,
 }
 // global variables
-const directions = [
-    FORWARD,
-    BACK,
-    LEFT,
-    RIGHT,
-    UP,
-    DOWN
-];
+const directions = [FORWARD, BACK, LEFT, RIGHT, UP, DOWN];
 
-const turns = [
-    LEFT_TURN,
-    RIGHT_TURN
-]
+const turns = [LEFT_TURN, RIGHT_TURN];
 
-
-//% block="Urban Pollution" weight=200 color=#4CAF50 icon="\uf1b9"
+//% block="Urban Pollution" weight=200 color=#FF0000 icon="\uf1b9"
 namespace urbanPollution {
+  /**
+   * Move Agent n spaces in the d direction
+   */
+  //% block="Move %d by %n"
+  export function moveAgent(d: Direction, n: number): void {
+    const direction = directions[d];
+    agent.move(direction, 1);
+  }
 
-    /**
-     * Move Agent n spaces in the d direction
-     */
-    //% block="Move %d by %n"
-    export function moveAgent(d: Direction, n: number): void {
-            const direction = directions[d];
-            agent.move(direction, 1);
-    }
-
-    /**
-     * Turn Agent in the t direction
-     */
-    //% block="Turn %t"
-    export function turnAgent(t: TurnDirection): void {
-        const turn = turns[t];
-        agent.turn(turn);
-    }  
-
+  /**
+   * Turn Agent in the t direction
+   */
+  //% block="Turn %t"
+  export function turnAgent(t: TurnDirection): void {
+    const turn = turns[t];
+    agent.turn(turn);
+  }
 }
